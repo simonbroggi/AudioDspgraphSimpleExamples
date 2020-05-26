531c261ea0f3364913831157fcd21e25c2a59ea1
Simon Broggi <simon.broggi@insert-coin.ch>
2020-05-26 14:28:19 +0200

    Waveform generation (#1)
    * sine wave
    
    * unity version update
    
    * ignore obj folder
    
    * add vscode package

---
+++ Assets/SimpleExamples/DSP/SinWaveNode.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Burst;
+using Unity.Mathematics;
+
+namespace Unity.Audio
+{
+    [BurstCompile]
+    public struct SinWaveNode : IAudioKernel<SinWaveNode.Parameters, SinWaveNode.Providers>
+    {
+        public enum Parameters
+        {
+            [ParameterDefault(300.0f)] [ParameterRange(200.0f, 1800.0f)]
+            Frequency
+        }
+
+        public enum Providers
+        {
+        }
+
+        float m_Phase;
+
+        public void Initialize()
+        {
+        }
+
+        public void Execute(ref ExecuteContext<Parameters, Providers> context)
+        {
+            if (context.Outputs.Count == 0)
+                return;
+
+            var outputBuffer = context.Outputs.GetSampleBuffer(0).Buffer;
+            var outputChannels = context.Outputs.GetSampleBuffer(0).Channels;
+
+            var frames = outputBuffer.Length / outputChannels;
+            var parameters = context.Parameters;
+            for (int s = 0, i = 0; s < frames; s++)
+            {
+                
+                float output = math.sin(m_Phase * math.PI);
+                for (var c = 0; c < outputChannels; c++)
+                {
+                    outputBuffer[i++] = output;
+                }
+                
+                float delta = parameters.GetFloat(Parameters.Frequency, s) / context.SampleRate;
+                m_Phase += delta;
+                m_Phase -= math.floor(m_Phase);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Assets/SimpleExamples/ScheduleParameter/ScheduleParametersWave.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Unity.Audio;
+using UnityEngine;
+
+public class ScheduleParametersWave : MonoBehaviour
+{
+    public float Frequency = 300.0f;
+    DSPGraph m_Graph;
+    DSPNode m_SineWave;
+
+    void Start()
+    {
+        var format = ChannelEnumConverter.GetSoundFormatFromSpeakerMode(AudioSettings.speakerMode);
+        var channels = ChannelEnumConverter.GetChannelCountFromSoundFormat(format);
+        AudioSettings.GetDSPBufferSize(out var bufferLength, out var numBuffers);
+        var sampleRate = AudioSettings.outputSampleRate;
+
+        m_Graph = DSPGraph.Create(format, channels, bufferLength, sampleRate);
+
+        var driver = new DefaultDSPGraphDriver { Graph = m_Graph };
+        driver.AttachToDefaultOutput();
+
+        using (var block = m_Graph.CreateCommandBlock())
+        {
+            m_SineWave = block.CreateDSPNode<SinWaveNode.Parameters, SinWaveNode.Providers, SinWaveNode>();
+            block.AddOutletPort(m_SineWave, 2, SoundFormat.Stereo);
+            block.Connect(m_SineWave, 0, m_Graph.RootDSP, 0);
+        }
+    }
+
+    void Update()
+    {
+        m_Graph.Update();
+    }
+
+    void OnDestroy()
+    {
+        using (var block = m_Graph.CreateCommandBlock())
+        {
+            block.ReleaseDSPNode(m_SineWave);
+        }
+    }
+
+    void OnGUI()
+    {
+        using (var block = m_Graph.CreateCommandBlock())
+        {
+            GUI.color = Color.white;
+            GUI.Label(new Rect(100, 70, 300, 30), "Frequency:");
+            var newFrequency = GUI.HorizontalSlider(new Rect(100, 100, 300, 30), Frequency, 200.0f, 2000.0f);
+            if (Math.Abs(newFrequency - Frequency) > 0.01f)
+            {
+                block.SetFloat<SinWaveNode.Parameters, SinWaveNode.Providers, SinWaveNode>(m_SineWave, SinWaveNode.Parameters.Frequency, newFrequency);
+                Frequency = newFrequency;
+            }
+        }
+    }
+}
